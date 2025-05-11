112f0f702c7f7e515a0ed63c78eb2a0174120447
Mustafa Genc <eposta@mustafagenc.info>
2025-05-11 14:44:48 +0300

    tum kurlar icin ornek hazirlandi

--- demo/src/app/all/page.tsx
+++ demo/src/app/all/page.tsx
@@ -1,6 +1,18 @@
-export default function Page() {
-    return <div className="m-auto w-3xl my-10">
-      tum kurlar
-    </div>;
+import { fetchAllCurrencies } from "kurlar";
+
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
+
+export default async function Page() {
+  const data = await fetchAllCurrencies();
+
+  if (!data) {
+    return <div className="m-auto w-3xl my-10">No data found</div>;
   }
-  
+
+  return (
+    <div className="m-auto w-3xl my-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}

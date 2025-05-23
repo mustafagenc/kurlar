d4970a0d0707a33af9d011d1eb9e64c08218e447
Mustafa Genç <eposta@mustafagenc.info>
2025-05-23 21:44:10 +0300

    analytics, speed-insights

--- demo/src/providers/theme-provider.tsx
+++ demo/src/providers/theme-provider.tsx
@@ -2,10 +2,13 @@
 
 import type * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return <NextThemesProvider {...props}>{children} <Analytics />
+    <SpeedInsights /></NextThemesProvider>;
 }
